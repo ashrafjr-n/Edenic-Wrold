61e3fe6934de69c129a7a17d3255be5db7850270
Ashraf <250116877+ashrafjr-n@users.noreply.github.com>
2026-08-27 00:11:14 +0400

    assemble the home page from hero, friends, paths and footer V6.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Hero } from "@/components/home/hero";
 import { FriendsSection } from "@/components/home/friends-section";
+import { PathsSection } from "@/components/home/paths-section";
+import { Footer } from "@/components/layout/footer";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col">
-      <Hero />
-      <FriendsSection />
-    </main>
+    <>
+      <main className="flex flex-1 flex-col">
+        <Hero />
+        <FriendsSection />
+        <PathsSection />
+      </main>
+      <Footer />
+    </>
   );
 }
